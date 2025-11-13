45e4853e767f74b2c26b29cc22d9a8542faecfcd
“edo98811” <filippie@uni-mainz.de>
2025-11-13 10:16:19 +0100

    added group mapping in edges

--- R/ggkegg_to_visnetwork.R
+++ R/ggkegg_to_visnetwork.R
@@ -47,6 +47,7 @@ kegg_to_graph <- function(path_id,
   nodes_df <- add_group(nodes_df)
   nodes_df <- nodes_df[order(nodes_df$label), ]
 
+  edges_df <- rbind(edges_df, add_group_relations(nodes_df)) # add group relations
   if (nrow(edges_df)) edges_df <- style_edges(edges_df)
 
   # --- 5. Build pathway name ---
@@ -152,8 +153,8 @@ map_results_to_nodes <- function(g, de_results, return_type = "visNetwork") {
         # visNetwork::visLegend(main = pathway_name, zoom = FALSE) %>%
         # visNetwork::visNodes(shape = nodes_df$shape_vis) %>%
         # visNetwork::visLegend(main = pathway_name, position = "left", zoom = FALSE) %>%
-        visNetwork::visOptions(highlightNearest = TRUE, nodesIdSelection = TRUE, selectedBy = "group")
-        # visNetwork::visSetTitle(main = pathway_name)
+        visNetwork::visOptions(highlightNearest = list(enabled = T, degree = 2, hover = T), nodesIdSelection = TRUE, selectedBy = "group")
+      # visNetwork::visSetTitle(main = pathway_name)
     },
     stop("Invalid return_type. Must be 'igraph' or 'visNetwork'.")
   )
@@ -285,7 +286,8 @@ style_edges <- function(edges_df) {
     glycosylation = list(color = "black", dashes = FALSE, arrows = "to", label = "+g"),
     ubiquitination = list(color = "black", dashes = FALSE, arrows = "to", label = "+u"),
     methylation = list(color = "black", dashes = FALSE, arrows = "to", label = "+m"),
-    others_unknown = list(color = "black", dashes = TRUE, arrows = "to", label = "?")
+    others_unknown = list(color = "black", dashes = TRUE, arrows = "to", label = "?"),
+    group_relation = list(color = "transparent", dashes = TRUE, arrows = "", label = "") # Invisible edges for group relations (only functional)
   )
 
   # https://builtin.com/data-science/and-in-r#:~:text=The%20single%20sign%20version%20%7C%20returns,first%20element%20of%20each%20vector.
@@ -299,6 +301,47 @@ style_edges <- function(edges_df) {
 
   return(edges_df)
 }
+
+#' Add edges representing group relationships between nodes
+#'
+#' @param nodes_df A tibble of nodes, must contain columns `name` and `group`.
+#' @return A tibble of edges representing pairwise relations between members of the same group.
+#' @importFrom tibble tibble
+#' @importFrom purrr map_dfr
+add_group_relations <- function(nodes_df) {
+  # Select nodes that belong to a group
+  group_nodes <- nodes_df[!is.na(nodes_df$group), , drop = FALSE]
+  unique_groups <- unique(group_nodes$group)
+
+  # Generate edges for each group
+  edges_df_group_relations <- purrr::map_dfr(unique_groups, function(g) {
+    # Node IDs for this group
+    group_members_node_id <- group_nodes$name[group_nodes$group == g] # name and id are the same
+    n <- length(group_members_node_id)
+
+    # Skip groups with only 1 member
+    # Concatenation with empty tibble will not add NA or empty rows
+    if (n < 2) return(tibble::tibble(from = character(0), 
+                                     to = character(0), 
+                                     type = character(0), 
+                                     subtype = character(0), 
+                                     rel_value = character(0)))
+
+    # Generate all pairwise combinations
+    # docs: https://www.rdocumentation.org/packages/utils/versions/3.6.2/topics/combn
+    pairs <- t(combn(group_members_node_id, 2))
+    tibble::tibble(
+      from = pairs[, 1],
+      to = pairs[, 2],
+      type = "group_relation",
+      subtype = "group_relation",
+      rel_value = NA_character_
+    )
+  })
+
+  edges_df_group_relations
+}
+
 
 #' Add results from combined results data frame to nodes data frame
 #' @param nodes_df Data frame of nodes with a column 'KEGG'
