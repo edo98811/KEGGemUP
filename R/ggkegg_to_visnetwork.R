340fcc095a436784c55ccc74fa727c3acb1724ff
“edo98811” <filippie@uni-mainz.de>
2026-01-06 13:32:43 +0100

    get_kegg_db called with argument names

--- R/ggkegg_to_visnetwork.R
+++ R/ggkegg_to_visnetwork.R
@@ -740,8 +740,8 @@ add_compound_names <- function(nodes_df, bfc) {
   compounds_in_graph[is.na(compounds_in_graph)] <- ""
   compounds_in_graph <- compounds_in_graph[idx]
 
-  compounds <- get_kegg_db(bfc, "compound") # expect named vector mapping KEGG id -> name
-  glycan <- get_kegg_db(bfc, "glycan") # expect named vector mapping KEGG id -> name
+  compounds <- get_kegg_db(db_name = "compound", bfc = bfc) # expect named vector mapping KEGG id -> name
+  glycan <- get_kegg_db(db_name = "glycan", bfc = bfc) # expect named vector mapping KEGG id -> name
 
   # safe lookup: if not found, use original id or empty string
   labels <- vapply(compounds_in_graph, function(id) {
