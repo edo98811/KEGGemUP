6f3e9cb0ddb8dae0270707397b9a8cd0b4b72650
Federico Marini <marinif@uni-mainz.de>
2026-04-08 12:25:15 +0200

    adding the package level documentation

---
+++ R/KEGGemUP-pkg.R
@@ -0,0 +1,18 @@
+#' KEGGemUP
+#'
+#' `KEGGemUP` is a Bioconductor package that provides a modern, interactive way
+#' to create, visualize, and explore KEGG pathways
+#'
+#' `KEGGemUP` spices up the KEGG pathways!
+#' Not just to simplify their exploration, but also to map your data and results
+#' onto the nodes and edges that constitute a KEGG pathway.
+#' `KEGGemUP` provides a cache-compatible approach to retrieve the underlying
+#' KGML files and avoid unnecessary download operations.
+#' Converting the KGML files into full igraph objects and embedding them into
+#' the visNetwork framework really brings your pathways to life, providing
+#' tooltips and additional information to be used e.g. in analysis reports or
+#' in fully fledged Shiny apps.
+#'
+#' @name KEGGemUP-pkg
+#' @keywords internal
+"_PACKAGE"
