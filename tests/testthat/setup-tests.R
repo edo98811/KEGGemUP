8ac6cda06d6d51ce388ab7336349ea8f08cf906c
Federico Marini <marinif@uni-mainz.de>
2026-04-22 19:30:47 +0200

    test suite runs live (does not take too much), still with some support on having the RDS archived in extdata

--- tests/testthat/setup-tests.R
+++ tests/testthat/setup-tests.R
@@ -1,9 +1,29 @@
+# loading required packages for setup -------------------------------------
+
 suppressMessages({
   library(KEGGemUP)
   library(igraph)
-  library(xml2) 
+  library(xml2)
   library(BiocFileCache)
 })
+
+# definition of expected files and co -------------------------------------
+
+kgml_path_01 <- system.file("extdata", "test01.xml", package = "KEGGemUP")
+kgml_path_broken <- system.file("extdata", "broken.xml", package = "KEGGemUP")
+kgml_path_empty <- system.file("extdata", "empty.xml", package = "KEGGemUP")
+kgml_path_no_edges <- system.file("extdata", "no_edges.xml", package = "KEGGemUP")
+
+xml_example <- xml2::read_xml(kgml_path_01)
+xml_no_edges <- xml2::read_xml(kgml_path_no_edges)
+
+bfc_path <- tools::R_user_dir("BiocFileCache", which = "cache")
+bfc <- BiocFileCache(cache = file.path(bfc_path, "test"), ask = FALSE)
+
+
+
+
+# specifying some nodes ---------------------------------------------------
 
 # existing nodes: 1111, 2222, 3333, K00001, tst00002, C00001, C00002, C00003
 # Existing genes and compounds (subset of the KGML)
@@ -17,7 +37,6 @@ nodes_C <- c("1111", "1111", "C00003", "C00002", "C99999")
 
 # All compounds including a non-existent one
 nodes_compounds <- c("C00001", "C00002", "C00003", "C00123")
-
 
 nodes_A_df <- data.frame(
   KEGGID = nodes_A,
@@ -38,6 +57,8 @@ nodes_compounds_df <- data.frame(
   log2FC = rnorm(length(nodes_compounds), mean = 0, sd = 1)
 )
 
+
+# DE-like content ---------------------------------------------------------
 
 # Mixed types and random naming
 de_results_list <- list(
@@ -63,99 +84,82 @@ de_results_list <- list(
   )
 )
 
-# throw_warning <- names(all_de_test_lists)[c(3, 4, 5)]
-# expected_warnings <- setNames(c(2, 2, 4), throw_warning)
-kgml_path_01 <- system.file("extdata", "test01.xml", package = "KEGGemUP")
-# kgml_path_real <- system.file("extdata", "hsa04010.xml", package = "KEGGemUP")
-# kgml_path_02 <- system.file("extdata", "test02.xml", package = "KEGGemUP")
-kgml_path_broken <- system.file("extdata", "broken.xml", package = "KEGGemUP")
-kgml_path_empty <- system.file("extdata", "empty.xml", package = "KEGGemUP")
-kgml_path_no_edges <- system.file("extdata", "no_edges.xml", package = "KEGGemUP")
 
-kgml_steps <- readRDS(system.file("extdata", "kgml_parsing_steps.rds", package = "KEGGemUP"))
-xml_example <- xml2::read_xml(kgml_path_01)
-xml_no_edges <- xml2::read_xml(kgml_path_no_edges)
-expected_graphs <- readRDS(system.file("extdata", "create_kegg_graph_expected.rds", package = "KEGGemUP"))
+
+# creating the rds objects for the testing --------------------------------
+
+## Tests for expected outputs of create_kegg_graph and map_results_to_graph
+
+expected_kgml_steps <- list()
+
+expected_kgml_steps$nodes <- parse_kgml_nodes(xml_example, kegg_vertex_defaults())
+expected_kgml_steps$groups <- parse_kgml_groups(xml_example, kegg_vertex_defaults())
+expected_kgml_steps$line_nodes <- parse_kgml_lines(xml_example, kegg_vertex_defaults())
+
+expected_kgml_steps$all_nodes <- rbind(
+  expected_kgml_steps$nodes,
+  expected_kgml_steps$groups,
+  expected_kgml_steps$line_nodes
+)
+
+expected_kgml_steps$relations_edges <- parse_kgml_relations(xml_example, kegg_edge_defaults())
+expected_kgml_steps$reactions_edges <- parse_kgml_reactions(xml_example, kegg_edge_defaults())
+expected_kgml_steps$line_edges <- parse_kgml_lines_edges(expected_kgml_steps$line_nodes,
+                                                kegg_edge_defaults())
+expected_kgml_steps$completed_reactions <-
+  complete_kgml_reactions(expected_kgml_steps$nodes, expected_kgml_steps$reactions_edges, kegg_edge_defaults())
+
+expected_kgml_steps$all_edges <- rbind(
+  expected_kgml_steps$relations_edges,
+  expected_kgml_steps$reactions_edges,
+  expected_kgml_steps$line_edges,
+  complete_kgml_reactions(expected_kgml_steps$nodes, expected_kgml_steps$reactions_edges, kegg_edge_defaults())
+)
+
 bfc_path <- tools::R_user_dir("BiocFileCache", which = "cache")
-bfc <- BiocFileCache(cache = file.path(bfc_path, "test"), ask = FALSE)
+bfc_map <- BiocFileCache(cache = file.path(bfc_path, "mappings"), ask = FALSE)
 
-# devtools::load_all()
+g_1 <- suppressWarnings({
+  build_kegg_graph(kgml_path_01, pathway_name = "hsa00001", bfc_map = bfc_map)
+})
 
-# # Tests for expected outputs of create_kegg_graph and map_results_to_graph
-# kgml_path_01 <- system.file("extdata", "test01.xml", package = "KEGGemUP")
-# # kgml_path_01 <- "inst/extdata/test01.xml"
-# xml_example <- xml2::read_xml(kgml_path_01)
-# kgml_steps <- list()
+expected_kgml_steps$g_test_01 <- g_1
 
-# kgml_steps$nodes <- parse_kgml_nodes(xml_example, kegg_vertex_defaults())
-# kgml_steps$groups <- parse_kgml_groups(xml_example, kegg_vertex_defaults())
-# kgml_steps$line_nodes <- parse_kgml_lines(xml_example, kegg_vertex_defaults())
+# saveRDS(expected_kgml_steps, file = "inst/extdata/expected_kgml_steps.RDS")
 
-# kgml_steps$all_nodes <- rbind(
-#   kgml_steps$nodes,
-#   kgml_steps$groups,
-#   kgml_steps$line_nodes
+g_test_01 <- suppressWarnings({
+  create_kegg_graph(
+    pathway_id = "hsa00001",
+    kgml_file = kgml_path_01
+  )
+})
+
+expected_output <- list(
+  g_test_01 = g_test_01
+)
+
+g_test_01_mapped <- suppressWarnings({
+  map_results_to_graph(
+    g = g_test_01,
+    de_results = de_results_list
+  )
+})
+
+expected_output$g_test_01_mapped <- g_test_01_mapped
+
+vis_graph_01 <- render_kegg_graph(g_test_01_mapped)
+
+expected_output$visNetwork_test_01_mapped <- vis_graph_01
+
+# saveRDS(expected_output, file = "inst/extdata/expected_output.RDS")
+
+# loading expected parsing steps and outputs - see above for the creation --------
+
+# expected_kgml_steps <- readRDS(
+#   system.file("extdata", "expected_kgml_steps.RDS", package = "KEGGemUP")
 # )
-
-# kgml_steps$relations_edges <- parse_kgml_relations(xml_example, kegg_edge_defaults())
-# kgml_steps$reactions_edges <- parse_kgml_reactions(xml_example, kegg_edge_defaults())
-# kgml_steps$line_edges <- parse_kgml_lines_edges(kgml_steps$line_nodes, kegg_edge_defaults())
-# kgml_steps$completed_reactions <-
-#   complete_kgml_reactions(kgml_steps$nodes, kgml_steps$reactions_edges, kegg_edge_defaults())
-
-# kgml_steps$all_edges <- rbind(
-#   kgml_steps$relations_edges,
-#   kgml_steps$reactions_edges,
-#   kgml_steps$line_edges,
-#   complete_kgml_reactions(kgml_steps$nodes, kgml_steps$reactions_edges, kegg_edge_defaults())
+# expected_output <- readRDS(
+#   system.file("extdata", "expected_output.RDS", package = "KEGGemUP")
 # )
-
-# bfc_path <- tools::R_user_dir("BiocFileCache", which = "cache")
-# bfc_map <- BiocFileCache(cache = file.path(bfc_path, "mappings"), ask = FALSE)
-
-# g_1 <- build_kegg_graph(kgml_path_01, pathway_name = "hsa00001", bfc_map = bfc_map)
-# # g_2 <- build_kegg_graph(kgml_path_02, pathway_name = "hsa00001", bfc_map = bfc_map)
-
-# kgml_steps$g_test_01 <- g_1
-# # kgml_steps$g_test_02 <- g_2
-
-# saveRDS(kgml_steps, file = "inst/extdata/kgml_parsing_steps.rds")
-
-# g_test_01 <- create_kegg_graph(
-#   pathway_id = "hsa00001",
-#   kgml_file = kgml_path_01
-# )
-
-# # g_test_02 <- create_kegg_graph(
-# #   pathway_id = "hsa00001",
-# #   kgml_file = kgml_path_02
-# # )
-
-# expected <- list(
-#   g_test_01 = g_test_01
-#   # g_test_02 = g_test_02
-# )
-
-# g_test_01_mapped <- map_results_to_graph(
-#   g = g_test_01,
-#   de_results = de_results_list
-# )
-
-# # g_test_02_mapped <- map_results_to_graph(
-# #   g = g_test_02,
-# #   de_results = de_results_list
-# # )
-
-# expected$g_test_01_mapped <- g_test_01_mapped
-# # expected$g_test_02_mapped <- g_test_02_mapped
-
-# vis_graph_01 <- render_kegg_graph(g_test_01_mapped)
-# # vis_graph_02 <- render_kegg_graph(g_test_02_mapped)
-
-# expected$visNetwork_test_01_mapped <- vis_graph_01
-# # expected$visNetwork_test_02_mapped <- vis_graph_02
-
-# saveRDS(expected, file = "inst/extdata/create_kegg_graph_expected.rds")
-
 
 
